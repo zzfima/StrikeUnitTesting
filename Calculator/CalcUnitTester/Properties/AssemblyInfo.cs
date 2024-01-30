cd4c27a8640eb9b8b68bed31bc2d2ed2014b80b1
Efim Zabarsky <EZ>
2024-01-30 13:32:30 +0200

    rfct

---
+++ Calculator/CalcUnitTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcUnitTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KLA Corp.")]
+[assembly: AssemblyProduct("CalcUnitTester")]
+[assembly: AssemblyCopyright("Copyright © KLA Corp. 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a36018e0-40c3-42ee-b3ad-5c2e7e37167a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Calculator/CalcUnitTester/UnitTest1.cs
+++ Calculator/CalcUnitTester/UnitTest1.cs
@@ -1,38 +1,39 @@
-using Calculator;
+﻿using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
-namespace TestProject1
+namespace CalcUnitTester
 {
 	[TestClass]
 	public class UnitTest1
 	{
+
 		[TestMethod]
 		public void TestMethodAdd()
 		{
 			ICalculator calculator = new SimpleCalculator();
-			Assert.AreEqual<double>(11, calculator.Add(7, 4));
+			Assert.AreEqual(11, calculator.Add(7, 4));
 		}
 
 		[TestMethod]
 		public void TestMethodMinus()
 		{
 			ICalculator calculator = new SimpleCalculator();
-			Assert.AreEqual<double>(3, calculator.Minus(7, 4));
+			Assert.AreEqual(3, calculator.Minus(7, 4));
 		}
 
 		[TestMethod]
 		public void TestMethodMultiply()
 		{
 			ICalculator calculator = new SimpleCalculator();
-			Assert.AreEqual<double>(28, calculator.Multiply(7, 4));
+			Assert.AreEqual(28, calculator.Multiply(7, 4));
 		}
 
 		[TestMethod]
 		public void TestMethodDivide()
 		{
 			ICalculator calculator = new SimpleCalculator();
-			Assert.AreEqual<double>(2, calculator.Divide(8, 4));
+			Assert.AreEqual(2, calculator.Divide(8, 4));
 		}
-
-
 	}
 }
